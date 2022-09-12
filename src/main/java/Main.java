ebc7b1b23b8c9a4c3221d5d2cd9bfe5e52757e69
SimonBD1 <simo20k3@stud.kea.dk>
2022-09-12 21:06:24 +0200

    Lavet en sout med s1, for at tjekke om den gemte de midlertidige informationer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Indtast den superhelt du leder efter eller opret en ny: ");
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.println("Vi kender ikke den superhelt, opret en ny superhelt");
         System.out.println("Indtast fornavn på Superhelt: ");
         String firstName = scanner.nextLine();
@@ -21,10 +22,12 @@ public class Main {
         String powers = scanner.nextLine();
         System.out.println("Indtast styrkeniveau:  ");
         double powerlvl = scanner.nextDouble();
-        System.out.println("Er den helt menneske:  ");
+        System.out.println("Er din helt menneske:  ");
         boolean race = scanner.nextBoolean();
 
         Superheroes s1 = new Superheroes (firstName,lastName,alias,powers,powerlvl,race);
+        System.out.println("Din helt hedder altså " + s1.getFirstName()+" "+s1.getLastName()+ " og går under navnet "+s1.getAlias()+" Hans superkræfter er: "+s1.getPowers() + " med et styrkeniveau på " + s1.getPowerlvl() + "Hans menneske status er " + s1.getRace());
+
 
     }
 }
